c8c0958d6e8eb5af2ac6477be2fd5d39ae9bfb14
benjamincarlin <bcarlin@broadinstitute.org>
2019-06-17 11:29:22 -0400

    renamed relationship table names, changed python file imports, updated relative python paths

--- test/unit_test_create_dataset.py
+++ test/unit_test_create_dataset.py
@@ -1,27 +1,26 @@
+#!/usr/bin/env python2
 import os
-import sys
-sys.path.insert(0, '/dsp-data-models/scripts')
 import unittest
 import tempfile
-import create_dataset
+from scripts.create_dataset import make_new_dataset
 import json
 from deepdiff import DeepDiff
 
 
 class CreateDatasetTest(unittest.TestCase):
-
     def test_files(self):
         # file paths for truth files
-        truth_output_schema_json = "./input_files/test_input_dataset.json"
+        current_file_dir = os.path.dirname(__file__)
+        truth_output_schema_json = os.path.join(current_file_dir, "input_files/test_input_dataset.json")
 
         # generate temp file names for output files
         output_dataset = os.path.join(tempfile.mkdtemp(), 'test_output_dataset')
 
         # run the create dataset python script with given input JSON files
-        create_dataset.make_new_dataset(study_name="study_name",
-                                        study_description="study_description",
-                                        parent_directory="input_files",
-                                        output_name=output_dataset)
+        make_new_dataset(dataset_name="dataset_name",
+                         dataset_description="dataset_description",
+                         parent_directory=os.path.join(current_file_dir, "input_files"),
+                         output_name=output_dataset)
 
         # compare expected outputs from python script to truth files
         with open(truth_output_schema_json, "r") as truth_file:
